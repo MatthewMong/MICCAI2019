89b0dfb4722812e6a49d3f84adce0936baddedba
Matthew Mong <matthew.mong1999@gmail.com>
2019-06-26 14:27:18 -0700

    Add Description

---
+++ Script.py
@@ -0,0 +1,3 @@
+# MICCAI 2019 Submission by UBC Biomedical Engineering Students
+# This repository is an attempt to identify and categorize
+# prostate tissue on the gleason score using machine learning and computer vision
