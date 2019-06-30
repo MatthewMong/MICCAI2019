4c13e18d2f8c964833eadee7473318bf1a5729e6
Roshan Pawar <steradian614@gmail.com>
2019-06-29 20:31:25 -0700

    Added initial structure and docs.

--- Script.py
+++ Script.py
@@ -1,3 +1,1 @@
-# MICCAI 2019 Submission by UBC Biomedical Engineering Students
-# This repository is an attempt to identify and categorize
-# prostate tissue on the gleason score using machine learning and computer vision
+""" Data Loading and Processing Scripts go here."""
