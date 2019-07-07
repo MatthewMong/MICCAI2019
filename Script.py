9d8746f2d6dd16a91da7e7850497ee7094c3b272
Matthew Mong <matthew.mong1999@gmail.com>
2019-07-07 16:16:35 -0700

    Add batch processing function

--- Script.py
+++ Script.py
@@ -1,16 +1,35 @@
 """ Data Loading and Processing Scripts go here."""
-import tensorflow as tf
 import os
+import glob
 from PIL import Image
 import numpy as np
+DEBUG=True
 
-#Should perform a batch extraction to filter all photos based on maps, will be completed soon
+#Examples of how to use:
+#batch_extract('D:/imgs/Test/Maps','D:/imgs/Test/Jpegs')
+#image_extract('D:/imgs/imgs/slide003_core004.jpg','D:/imgs/Maps1/maps1/slide003_core004_classimg_nonconvex.png')
+
+
+#Should perform a batch extraction to filter all photos based on maps, feed it the directory with your png maps, and your jpg locations
 def batch_extract(png_location,jpg_location):
-    print('blank right now, check back later')
+    dict = {0: [], 1: [], 2: [], 3: [], 4: [], 5: []}
+    jpgs=os.listdir(jpg_location)
+    for image in jpgs:
+        value=glob.glob(png_location+'/'+image.strip('.jpg')+'_classimg_nonconvex.png')
+        if(len(value)==1):
+            eximage=image_extract(jpg_location+'/'+image,png_location+'/'+image.strip('.jpg')+'_classimg_nonconvex.png')
+            dict[eximage[0]].append(eximage[1])
+        elif(len(value)==0):
+            print("no file with that name")
+        else:
+            print('error, more than one file with same name exists')
+    return dict
 
-#this function extracts the image based on the maps we are given, it serves as a helper function to batch_extract
+#this function extracts the image based on the maps we are given, it serves as a helper function to batch_extract, it does not differentiate between different levels within an image
+#both inputs should be the locations of your png and jpg files
 def image_extract(jpg,png):
     img_map=Image.open(png)
+    returnval=[]
     with open(jpg,'rb') as img_file:
         img_file.seek(163)
         a=img_file.read(2)
@@ -20,18 +39,22 @@ def image_extract(jpg,png):
     img=Image.open(jpg)
     img_map_arr=np.array(img_map,dtype='uint8')
     img_arr=np.array(img,dtype='uint8')
-    print(img_arr[12,4])
-    pls=Image.fromarray(img_arr)
-    pls.show()
+    #print(img_arr[12,4])
+    #pls=Image.fromarray(img_arr)
+    #pls.show()
     abz=img_map_arr.flatten()
     abz2=img_arr.reshape(width*height,3)
     abz2copy=np.empty(abz2.shape)
+    value=np.unique(abz)
+    value=value[1]
     for x in abz.nonzero():
         abz2copy[x]=abz2[x]
     abz2copy=abz2copy.reshape(height,width,3)
     img_arr=abz2copy
     final=Image.fromarray(img_arr.astype('uint8'))
-    final.show()
+    if DEBUG==True:
+        final.show()
+    returnval.append(value)
+    returnval.append(final)
+    return returnval
 
-
-#image_extract('D:/imgs/imgs/slide003_core004.jpg','D:/imgs/Maps1/maps1/slide003_core004_classimg_nonconvex.png')
